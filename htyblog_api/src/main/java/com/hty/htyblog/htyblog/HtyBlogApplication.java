2b86dc9888e4a3986d16a5524911745ac163dc3e
huangtianyou <1156388927@qq.com>
2022-07-29 15:23:12 +0800

    第一次提交

---
+++ htyblog_api/src/main/java/com/hty/htyblog/htyblog/HtyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.hty.htyblog.htyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtyBlogApplication.class, args);
+    }
+
+}
---
+++ htyblog_api/src/test/java/com/hty/htyblog/htyblog/HtyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hty.htyblog.htyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
